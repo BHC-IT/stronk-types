79b5869a8918fe1deabde56806c6415baff56938
Ludovic Fernandez <ludofernandez@msn.com>
2023-05-31 01:24:53 +0200

    style(Conditionals): Prettify with new width rule

--- types/Conditionals.d.ts
+++ types/Conditionals.d.ts
@@ -9,7 +9,11 @@ export type NotExtends<Type, ToExtend> = Not<Extends<Type, ToExtend>>
 export type Not<Test extends boolean> = If<Test, false, true>
 export type And<A extends boolean, B extends boolean> = If<A, If<B, true, false>, false>
 export type Or<A extends boolean, B extends boolean> = If<A, true, If<B, true, false>>
-export type Xor<A extends boolean, B extends boolean> = If<A, If<Not<B>, true, false>, If<B, true, false>>
+export type Xor<A extends boolean, B extends boolean> = If<
+  A,
+  If<Not<B>, true, false>,
+  If<B, true, false>
+>
 
 export type NeverIf<Test extends boolean, OnTrue = unknown> = If<Test, never, OnTrue>
 export type NeverIfNot<Test extends boolean, OnFalse = unknown> = If<Not<Test>, never, OnFalse>
@@ -17,5 +21,13 @@ export type NeverIfNot<Test extends boolean, OnFalse = unknown> = If<Not<Test>, 
 export type IsNever<Type> = [Type] extends [never] ? true : false
 export type IsNotNever<Type> = Not<IsNever<Type>>
 
-export type IfNever<Type, OnIsNever, OnIsNotNever = never> = If<IsNever<Type>, OnIsNever, OnIsNotNever>
-export type IfNotNever<Type, OnIsNotNever, OnIsNever = never> = If<Not<IsNever<Type>>, OnIsNotNever, OnIsNever>
+export type IfNever<Type, OnIsNever, OnIsNotNever = never> = If<
+  IsNever<Type>,
+  OnIsNever,
+  OnIsNotNever
+>
+export type IfNotNever<Type, OnIsNotNever, OnIsNever = never> = If<
+  Not<IsNever<Type>>,
+  OnIsNotNever,
+  OnIsNever
+>
