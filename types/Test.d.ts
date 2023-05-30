f2d166704fdafa14d1fef7d5f20a4c3ae396d027
Ludovic Fernandez <ludofernandez@msn.com>
2023-05-30 19:39:36 +0200

    feat(Test): Add ExpectNotExtends

--- types/Test.d.ts
+++ types/Test.d.ts
@@ -1,12 +1,14 @@
 // @ts-nocheck These types cause TS errors when not used in a testing context
 
 import { Expect, ExpectTrue, ExpectFalse, ExpectExtends, ExpectValidArgs, Equal, NotEqual } from '@type-challenges/utils'
-import { IsNever, IsNotNever } from './Conditionals'
+import { IsNever } from './Conditionals'
 
 export { Expect, ExpectTrue, ExpectFalse, ExpectExtends, ExpectValidArgs, Equal, NotEqual }
 
 export type ExpectNever<T> = ExpectTrue<IsNever<T>>
-export type ExpectNotNever<T> = ExpectTrue<IsNotNever<T>>
+export type ExpectNotNever<T> = Not<ExpectNever<T>>
 
 export type ExpectEqual<T, U> = Expect<Equal<T, U>>
-export type ExpectNotEqual<T, U> = Expect<NotEqual<T, U>>
+export type ExpectNotEqual<T, U> = Not<ExpectEqual<T, U>>
+
+export type ExpectNotExtends<T, U> = Not<ExpectExtends<T, U>>
