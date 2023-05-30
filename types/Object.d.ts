9fd75ce900e2f27013088553bc42620973373e53
Ludovic Fernandez <ludofernandez@msn.com>
2023-05-31 01:49:45 +0200

    feat(Object): Add IsKnown

--- types/Object.d.ts
+++ types/Object.d.ts
@@ -1,6 +1,9 @@
 import { IfNotAny, IfNotNever, IsNotAny, IsNotNever } from './Conditionals'
 
 export namespace Object {
+  /** Check that an object type is neither `never` not `any` */
+  export type IsKnown<T extends object> = IsNotNever<IsNotNever<T> & IsNotAny<T>>
+
   /** Extract an object's keys as union */
   export type Keys<T extends object> = IfNotNever<T, IfNotAny<T, keyof T>>
 }
