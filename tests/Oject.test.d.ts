2c7e30d83ab135d896076c4b43f9db6bcda91ce0
Ludovic Fernandez <ludofernandez@msn.com>
2023-05-31 01:51:11 +0200

    test(Object): Add tests for IsKnown and Entry

--- tests/Oject.test.d.ts
+++ tests/Oject.test.d.ts
@@ -3,8 +3,24 @@ import { Equal, Expect, ExpectEmpty, ExpectFalse, ExpectNever, ExpectTrue } from
 import '../types'
 
 declare module '../types' {
+  namespace Object {
+    export interface Dummy {
+      aString: string
+      aNumber: number
+      anObject: {
+        prop1: string
+        prop2: string
+      }
+    }
+
     // prettier-ignore
     type __TEST__ = {
+        IsKnown: [
+          ExpectTrue<IsKnown<Dummy>>,
+          ExpectTrue<IsKnown<{}>>,
+          ExpectFalse<IsKnown<any>>,
+          ExpectFalse<IsKnown<never>>
+        ]
         Keys: [
           Expect<Equal<Keys<Dummy>, 'aString' | 'aNumber' | 'anObject'>>,
           ExpectNever<Keys<{}>>,
@@ -13,6 +29,11 @@ declare module '../types' {
           // @ts-expect-error
           Keys<'sauce'>,
         ],
+        Entry: [
+          Expect<Equal<Entry<Dummy, 'aString'>, ['aString', Dummy['aString']]>>,
+          Expect<Equal<Entry<Dummy, 'aNumber'>, ['aNumber', Dummy['aNumber']]>>,
+          Expect<Equal<Entry<Dummy, 'anObject'>, ['anObject', Dummy['anObject']]>>,
+        ]
       }
   }
 }
