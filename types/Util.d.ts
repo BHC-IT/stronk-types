6a653cc024f23c7aed8b23f5f467e6d4e1b5bcd3
Ludovic Fernandez <ludofernandez@msn.com>
2023-06-17 21:08:42 +0200

    refactor(Util): Add `boolean` to `Primitive` union

--- types/Util.d.ts
+++ types/Util.d.ts
@@ -1,8 +1,4 @@
-import { MergeInsertions } from "@type-challenges/utils"
-
-export { MergeInsertions }
-
-export type Primitive = string | number
+export type Primitive = string | number | boolean
 export type Accessor = keyof any
 
 export type Nothing = Record<Accessor, never>
@@ -32,6 +28,12 @@ export namespace Opaque {
    * @template Tag A unique string indetifier to differentiate this {@link Opaque} from others
    */
   export type Strict<T, Tag extends string> = T & Tagged<Tag>
+}
+
+export type MergeInsertions<T> = T extends object ? { [key in keyof T]: T[key] } : T
+
+export namespace MergeInsertions {
+  export type Deep<T> = T extends object ? { [key in keyof T]: Deep<T> } : MergeInsertions<T>
 }
 
 export type PartialExcept<T, K extends keyof T>
